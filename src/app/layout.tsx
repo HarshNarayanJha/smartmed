d8df6f92828286dc46182835449ac6d359ce2d8e
Harsh Narayan Jha <harshnj@proton.me>
2025-04-03 23:54:39 +0530

    fix: init lit only in production environment

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -35,7 +35,6 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode
 }>) {
-  console.log("Intializing Lit")
   return (
     <html lang="en" suppressHydrationWarning>
       <head />
--- src/components/reusable/LitAnalytics.tsx
+++ src/components/reusable/LitAnalytics.tsx
@@ -4,12 +4,16 @@ import { Lit } from "litlyx-js"
 import { useEffect } from "react"
 
 const LIT_CONFIG = {
-  projectId: process.env.NEXT_PUBLIC_LIT_PROJECT_ID
+  projectId: process.env.NEXT_PUBLIC_LIT_PROJECT_ID,
+  nodeEnv: process.env.NODE_ENV
 }
 
 export default function LitAnalytics() {
   useEffect(() => {
-    Lit.init(LIT_CONFIG.projectId)
+    if (LIT_CONFIG.nodeEnv === "production") {
+      console.log("Intializing Lit")
+      Lit.init(LIT_CONFIG.projectId)
+    }
   }, [])
 
   return null
